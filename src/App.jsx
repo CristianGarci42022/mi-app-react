27260df7b0be0606bb919a324765289e98f4f3ee
Cristian Garcia <107603821+CristianGarci42022@users.noreply.github.com>
2024-01-12 16:57:15 -0500

    Ajuste de colores

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import './App.css';
 import './styles/Login.css';
 import './styles/profile.css';
 import { Home } from "../src/Components/Pages/Home";  
